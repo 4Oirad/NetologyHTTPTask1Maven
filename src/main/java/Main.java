792362b74898b47f4cbf39912de834f3f5675d39
Bair Arshaev <ars-bair@yandex.ru>
2022-06-16 00:11:54 +0300

    Edit code 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,19 +16,14 @@ public class Main {
             "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
 
     public static void main(String[] args) throws IOException {
-
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
-                .setDefaultRequestConfig(RequestConfig.custom()
-                        .setConnectTimeout(5000)
-                        .setSocketTimeout(30000)
-                        .setRedirectsEnabled(false)
-                        .build())
-                .build();
-        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
-        CloseableHttpResponse response = httpClient.execute(request);
-        List<Post> posts = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<Post>>() {});
-        posts.stream()
-                .filter(x -> x.getUpvotes() != null && x.getUpvotes() > 0)
-                .forEach(System.out::println);
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()){
+            HttpUriRequest httGet = new HttpGet(REMOTE_SERVICE_URI);
+            try (CloseableHttpResponse response = httpClient.execute(httGet)){
+                List<Post> posts = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<Post>>() {});
+                posts.stream()
+                    .filter(x -> x.getUpvotes() != null && x.getUpvotes() > 0)
+                    .forEach(System.out::println);
+            }
+        }
     }
 }
